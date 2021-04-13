a2b724d05079b23e01093b4fd52d67e1c2aaa7ec
Jaime Pastor <jaime,pastor6@educa.madrid.org>
2021-04-13 13:19:11 +0200

    Cambio realizado desde dirCasa

--- Diferencia_horaria.java
+++ Diferencia_horaria.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 //Cambio hecho desde dirCasa
 //Cambio hecho desde dirInsti
+//Segundo cambio hecho desde dirCasa esta vez para crear conflictos
 public class Diferencia_horaria {
     static Scanner sc = new Scanner(System.in);
 
